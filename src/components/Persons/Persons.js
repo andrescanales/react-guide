5c985c2c58b8614ae79b27a9c1c264251d35b8b5
Andrés <andres.canales66@gmail.com>
2022-03-03 17:46:28 -0600

    Transforming functional components into class components to apply the update React lifecycle

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,19 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
 import ErrorBoundary from '../ErrorBoundary/ErrorBoundary';
 
-const persons = (props) => (
-  props.persons.map((person, index) => {
-    // We moved the key prop to the HigherComponent
-    // in this case the ErrorBoundary component.
-    return <ErrorBoundary key={person.id}>
-      <Person
-        name={person.name}
-        age={person.age}
-        click={() => props.clicked(index)}
-        changed={(event) => props.changed(event, person.id)}/>
-      </ErrorBoundary>
-  })
-);
+// const persons = (props) => (
+class Persons extends Component {
+  static getDerivedStateFromProps(props, state){
+    console.log('[Persons.js] getDerivedStateFromProps')
+    return state;
+  }
 
-export default persons;
+  shouldComponentUpdate(nextProps, nextState){
+    console.log('[Persons.js] shouldComponentUpdate')
+    // We need to return true or false, no matter what.
+    // true if React should continue updating, false otherwise.
+    // Obviouslly this will occurr while doing a validation in 
+    // this.props to the upcoming nextProps.
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState){
+    console.log('[Persons.js] getSnapshotBeforeUpdate')
+    // This is the way to use this method
+    return {message: 'Snapshot'};
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot){
+    console.log('[Persons.js] componentDidUpdate')
+    console.log(snapshot)
+  }
+
+  render() {
+    console.log('[Persons.js] rendering...')
+    return(
+      this.props.persons.map((person, index) => {
+        // We moved the key prop to the HigherComponent
+        // in this case the ErrorBoundary component.
+        return <ErrorBoundary key={person.id}>
+          <Person
+            name={person.name}
+            age={person.age}
+            click={() => this.props.clicked(index)}
+            changed={(event) => this.props.changed(event, person.id)}/>
+          </ErrorBoundary>
+      })
+    )
+  }
+}
+
+export default Persons;
