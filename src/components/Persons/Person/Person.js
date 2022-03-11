6aa52939c4b3b0d1b54309cc3858fded4b7c1785
Andrés <andres.canales66@gmail.com>
2022-03-10 21:42:26 -0600

    Using higher order components to wrap other components

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
 import './Person.css'
+import Aux from '../../../hoc/Aux';
+// Using a higher order component just to avoid the need
+// of using a HTML tag wrapping all the JSX code
 
 // const person = (props) => {
 class Person extends Component {
   render() {
     console.log('[Person.js] rendering...')
     return (
-      <div className="Person">
+      <Aux>
+      {/* {<div className="Person">} */}
         <p onClick={this.props.click}>I'm a {this.props.name} and I am {this.props.age} years old!</p>
         {/* props.children is used to display whatever info is included in
           the opening and closing tags when invoking a component. */} 
         <p>{this.props.children}</p>
         <input type="text" onChange={this.props.changed} value={this.props.name}/>
-      </div>
+      {/* {</div>} */}
+      </Aux>
     )
   }
 }
---
+++ src/hoc/Aux.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+// Remember that props.children is basically whatever is 
+// inside the render method in the components that is using it
+const aux = props => props.children;
+
+export default aux
